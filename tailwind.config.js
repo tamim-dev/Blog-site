1a49fd0663347f083c34e91ae5051a674a6e5ccc
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2024-01-24 01:43:51 +0600

    banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                ru: "'Helvetica Neue', sans-serif",
+                ru: "'Helvetica Neue'",
             },
             spacing: {
                 container: "1230px",
